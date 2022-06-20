d2ea04e14c05f8e1b8978d485a19796fad329a4f
Elián Farias <elianfarias0@gmail.com>
2022-06-20 20:50:03 -0300

    add project solution/problem single responsability

---
+++ SingleResponsability/ProblemSingleResponsability/ProblemSingleResponsability/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProblemSingleResponsability.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
